924ea4ef98e44be83736524afc9784f0719a3a9c
KainSlow <jorge_alfonso3@hotmail.com>
2025-03-23 05:46:38 -0300

    Added alert when liking a button.
    Added:
    -Alert when liking a definition.
    
    Change:
    -Definition panels to be a <form> instead of a <div>

--- src/app.js
+++ src/app.js
@@ -6,12 +6,28 @@ var define_button;
 function addLikeEvent(element) {
     element.addEventListener("click",
         function () {
+            
+            // Like count
+
             let html = this.innerHTML;
             let num_index = html.search(/\d/i);
             html = html.slice(num_index);
             html = html.split(/\s/g);
             let like_count = Number(html[0]);
             this.innerHTML = (++like_count) + " me gusta";
+
+            // Alert
+            //
+            // There is two ways to do this, one is using the .parentNode property
+            // to access the parent element then its first child,
+            // which is the one containing the animal's name.
+            // The second option requires the parent of the button to be a form element,
+            // which allow us to access it through the .form property, the rest is the same as the first option.
+            
+            //let title = this.form.children[0].innerHTML;
+            let title = this.parentNode.children[0].innerHTML;
+
+            alert(title + " was liked.");
         });
 }
 
